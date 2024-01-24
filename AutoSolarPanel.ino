5647d331f49de61c059434258169c31927713b50
AndreyBritvin <38183643+AndreyBritvin@users.noreply.github.com>
2024-01-24 18:48:25 +0300

    Better algorithm

--- AutoSolarPanel.ino
+++ AutoSolarPanel.ino
@@ -2,15 +2,15 @@
 #define PHOTO_SENS_2 A1
 #define PHOTO_SENS_3 A2
 #define PHOTO_SENS_4 A3
-#define PHOTO_SENS_PLAT_1 A4
-#define PHOTO_SENS_PLAT_2 A5
+#define PHOTO_SENS_5 A4
+#define PHOTO_SENS_6 A5
 
 #define PHOTO_ENABLE 9
 
 #define SERVO_DOWN_PIN 7
 #define SERVO_UP_PIN 8
 
-int sens_1, sens_2, sens_3, sens_4, sens_plat_1, sens_plat_2;
+int sens_1, sens_2, sens_3, sens_4, sens_5, sens_6;
 
 #include <Servo.h>
 
@@ -23,13 +23,13 @@ int need_pos = 0;
 
 void setup() {
  Serial.begin(115200);
-pinMode(PHOTO_SENS_1, INPUT);
-pinMode(PHOTO_SENS_2, INPUT);
-pinMode(PHOTO_SENS_3, INPUT);
-pinMode(PHOTO_SENS_4, INPUT);
+pinMode(PHOTO_SENS_1, INPUT_PULLUP);
+pinMode(PHOTO_SENS_2, INPUT_PULLUP);
+pinMode(PHOTO_SENS_3, INPUT_PULLUP);
+pinMode(PHOTO_SENS_4, INPUT_PULLUP);
 
-pinMode(PHOTO_SENS_PLAT_1, INPUT);
-pinMode(PHOTO_SENS_PLAT_2, INPUT);
+pinMode(PHOTO_SENS_5, INPUT_PULLUP);
+pinMode(PHOTO_SENS_6, INPUT_PULLUP);
 
 pinMode(PHOTO_ENABLE, OUTPUT);
 digitalWrite(PHOTO_ENABLE, LOW);
@@ -39,20 +39,25 @@ myservo_up.attach(SERVO_UP_PIN);
 }
 
 void loop() {
+ get_sens_data();
+debug_print();
+
+delay(1000);
+}
+
+void get_sens_data(void)
+{
+  
 digitalWrite(PHOTO_ENABLE, HIGH);
 delay(15);
 sens_1 = analogRead(PHOTO_SENS_1);
 sens_2 = analogRead(PHOTO_SENS_2);
 sens_3 = analogRead(PHOTO_SENS_3);
 sens_4 = analogRead(PHOTO_SENS_4);
-
+sens_5 = analogRead(PHOTO_SENS_5);
+sens_6 = analogRead(PHOTO_SENS_6);
 digitalWrite(PHOTO_ENABLE, LOW);
-debug_print();
-sens_plat_1 = analogRead(PHOTO_SENS_PLAT_1);
-sens_plat_2 = analogRead(PHOTO_SENS_PLAT_2);
-delay(1000);
 }
-
 
 void debug_print(void)
 {
@@ -64,8 +69,9 @@ Serial.print(sens_3);
 Serial.print(" ");
 Serial.print(sens_4);
 Serial.print(" ");
-Serial.print(sens_plat_1);
+Serial.print(sens_5);
 Serial.print(" ");
-Serial.print(sens_plat_1);
-Serial.print(" ");
+Serial.print(sens_6);
+Serial.println(" ");
+
 }
